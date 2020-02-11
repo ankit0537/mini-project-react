54f016b15e4285662ed6db276c66630fead731b2
ankit kumar <ankitsistecr@gmail.com>
2020-02-12 00:43:26 +0530

    Updates

---
+++ precache-manifest.122a22f61069ef22cae4d55ee962eaf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4275f53bd0d65e0eefab62ab1a1b24a9",
+    "url": "/mini-project-react/index.html"
+  },
+  {
+    "revision": "2431db29e8d9b374deb9",
+    "url": "/mini-project-react/static/css/main.855623c5.chunk.css"
+  },
+  {
+    "revision": "f5bd15c2c5cde5a86396",
+    "url": "/mini-project-react/static/js/2.26c2d070.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mini-project-react/static/js/2.26c2d070.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2431db29e8d9b374deb9",
+    "url": "/mini-project-react/static/js/main.1cf2f70d.chunk.js"
+  },
+  {
+    "revision": "09a627155cb26e02d346",
+    "url": "/mini-project-react/static/js/runtime-main.dbd3864f.js"
+  }
+]);
